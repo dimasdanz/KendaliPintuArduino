a8511719a14f49c68280843ea1a80dcb06d7f263
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-24 04:54:11 +0700

    Add LCD Function

--- KendaliPintuArduino.ino
+++ KendaliPintuArduino.ino
@@ -19,16 +19,16 @@ const byte ROWS = 4;
 const byte COLS = 3;
 char keys[ROWS][COLS] = {
   {
-    '1','2','3'    }
+    '1','2','3'        }
   ,
   {
-    '4','5','6'    }
+    '4','5','6'        }
   ,
   {
-    '7','8','9'    }
+    '7','8','9'        }
   ,
   {
-    '*','0','#'    }
+    '*','0','#'        }
 };
 
 byte rowPins[ROWS] = {
@@ -43,6 +43,7 @@ Servo myservo;
 char pass[10];
 int attempt = 0;
 int count = 0;
+String disp = "";
 
 const int sensor = A2;
 const int push_btn = A3;
@@ -53,15 +54,26 @@ long idleTime = 0;
 long interval_reset = 180000;
 long interval_idle = 20000;
 
+boolean disp_online = false;
+boolean disp_lock = false;
+boolean disp_offline = false;
+boolean disp_inactive = false;
+
 void setup(){
   Serial.begin(9600);
   myservo.attach(9);
+
   Ethernet.begin(mac, ip);
   server.begin();
+
+  lcd.init();
+  lcd.backlight();
+
   pinMode(sensor, INPUT);
   digitalWrite(sensor, HIGH);
   pinMode(push_btn, INPUT);
   digitalWrite(push_btn, HIGH);
+
   myservo.write(70);
   Serial.println("Ready");
 }
@@ -76,6 +88,10 @@ void loop(){
             char type = client.read();
             if(type == 'o') {
               Serial.println("Open Command");
+              /*if(!disp_offline){
+               disp_offline = true;
+               lcd_offline(false);
+               }*/
               client.println("HTTP/1.1 200 OK");
               client.println("Content-Type: application/json");
               client.println();
@@ -87,7 +103,7 @@ void loop(){
               Serial.println("Status Command");
               if(val == 2){
                 device_status = true;
-                Serial.println("Activate");
+                lcd_init();
                 client.println("HTTP/1.1 200 OK");
                 client.println("Content-Type: application/json");
                 client.println();
@@ -156,30 +172,41 @@ void loop(){
     auth_user("keluar");
     open_door();
   }
-  
-  if(device_status && !device_isLock()){
-    char key = keypad.getKey();
-    if (key != NO_KEY && key != '*' && key != '#'){
-      isInputting = true;
-      pass[count] = key;
-      count++;
-    }
-    if(key == '*'){
-      isInputting = false;
-      attempt++;
-      auth_user(pass);
-      sys_init();
-    }
-    if(key == '#'){
-      sys_init();
-    }
-    if(count > 9){
-      auth_user(pass);
-      sys_init();
+
+  if(device_status){
+    if(!device_isLock()){
+      char key = keypad.getKey();
+      if (key != NO_KEY && key != '*' && key != '#'){
+        idleTime = millis();
+        isInputting = true;
+        disp += "*";
+        lcd.setCursor(0,2);
+        lcd.print(disp);
+        pass[count] = key;
+        count++;
+      }
+      if(key == '*'){
+        idleTime = millis();
+        isInputting = false;
+        attempt++;
+        auth_user(pass);
+        sys_init();
+      }
+      if(key == '#'){
+        idleTime = millis();
+        sys_init();
+      }
+      if(count > 9){
+        auth_user(pass);
+        sys_init();
+      }
     }
   }
   else{
-    //Device Non-Active
+    if(!disp_inactive){
+      disp_inactive = true;
+      lcd_print("Perangkat Non-Aktif");
+    }
   }
 
   unsigned long curTime = millis();
@@ -193,6 +220,10 @@ void loop(){
     if(curTime - prevTime > interval_reset){
       prevTime = curTime;
       attempt = 0;
+      lcd.setCursor(0,3);
+      for(int i=0;i<20;i++){
+        lcd.print(" ");
+      }
     }
   }
 }
@@ -202,6 +233,11 @@ void sys_init(){
   Serial.println(pass);
   memset(pass, 0, 10);
   count = 0;
+  disp = "";
+  lcd.setCursor(0,2);
+  for(int i=0;i<20;i++){
+    lcd.print(" ");
+  }
   Serial.println(pass);
 }
 
@@ -219,6 +255,10 @@ void auth_user(char user_input[]){
     Serial.println("Sending success");
   }
   else{
+    /*if(!disp_offline){
+     disp_offline = true;
+     lcd_offline(true);
+     }*/
     Serial.println("Sending failed");
     String pass(user_input);
     if(pass == "123"){
@@ -233,29 +273,109 @@ void auth_user(char user_input[]){
 }
 
 void open_door(){
-  Serial.println("Door open");
+  lcd_print("Pintu Terbuka");
   attempt = 0;
   myservo.write(30);
   delay(3000);
   while(digitalRead(sensor) == HIGH);
   delay(1000);
   myservo.write(70);
-  Serial.println("Door closed");
+  lcd_init();
   idleTime = millis();
 }
 
 boolean device_isLock(){
   if(attempt >= 3){
-    //Serial.println("Device lock");
+    if(!disp_lock){
+      lcd_print("Perangkat Terkunci");
+      lcd.setCursor(0,3);
+      lcd.print("Tunggu 30 detik");
+      disp_lock = true;
+      disp_online = false;
+      disp_inactive = false;
+    }
     return true;
   }
   else{
-    //Serial.println("Device unlock");
+    if(!disp_online){
+      lcd_init();
+      disp_online = true;
+      disp_lock = false;
+      disp_inactive = false;
+    }
     return false;
   }
 }
 
 void wrong_password(){
+  if(!disp_lock){
+    lcd_print("Password Salah");
+    lcd_attempts();
+  }
   Serial.println("Wrong Password");
+  delay(1000);
+  /*if(!device_isLock()){
+    lcd_init();
+  }*/
   idleTime = millis();
 }
+
+void lcd_init(){
+  lcd.setCursor(0,1);
+  for(int i=0;i<20;i++){
+    lcd.print(" ");
+  }
+  lcd.setCursor(0,2);
+  for(int i=0;i<20;i++){
+    lcd.print(" ");
+  }
+  lcd.setCursor(3,0);
+  lcd.print("Kendali Pintu");
+  lcd.setCursor(0,1);
+  lcd.print("Kata Kunci :");
+  lcd.setCursor(0,2);
+  for(int i=0;i<20;i++){
+    lcd.print(" ");
+  }
+}
+
+void lcd_print(String s){
+  disp_online = false;
+  disp_lock = false;
+  lcd.setCursor(0,1);
+  for(int i=0;i<20;i++){
+    lcd.print(" ");
+  }
+  lcd.setCursor(0,2);
+  for(int i=0;i<20;i++){
+    lcd.print(" ");
+  }
+  lcd.setCursor(0,3);
+  for(int i=0;i<20;i++){
+    lcd.print(" ");
+  }
+  lcd.setCursor(0,1);
+  lcd.print(s);
+}
+
+void lcd_attempts(){
+  lcd.setCursor(0,3);
+  lcd.print("Percobaan :");
+  lcd.setCursor(13,3);
+  lcd.print(attempt);
+  lcd.setCursor(15,3);
+  lcd.print("/3");
+}
+
+void lcd_offline(boolean b){
+  if(b){
+    lcd.setCursor(19,0);
+    lcd.print("*");
+  }
+  else{
+    lcd.setCursor(19,0);
+    lcd.print(" ");
+  }
+}
+
+
