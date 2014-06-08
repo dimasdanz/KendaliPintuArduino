890d42f853c09e56ba7cdca9c36345b3ac1f7264
Dimas R. Danu <dimasdanz@gmail.com>
2014-06-08 09:56:07 +0700

    Add backup power conditional

--- KendaliPintuArduino.ino
+++ KendaliPintuArduino.ino
@@ -24,10 +24,8 @@
 #include <Servo.h>
 
 const char server_addr[] = "192.168.2.4";
-byte mac[] = {
-  0x90, 0xA2, 0xDA, 0x0E, 0xF5, 0xF8};
-byte ip[] = {
-  192,168,1,73};
+byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0xF5, 0xF8};
+byte ip[] = {192,168,1,73};
 
 EthernetServer server(80);
 boolean device_status = true;
@@ -56,6 +54,7 @@ String disp = "";
 const int sensor = A2;
 const int push_btn = A3;
 const int servo = 9;
+const int primary_power = A0;
 
 long prevTime = 0;
 long idleTime = 0;
@@ -67,6 +66,7 @@ boolean disp_lock = false;
 boolean disp_offline = false;
 boolean disp_online = false;
 boolean disp_inactive = false;
+boolean backup_power = false;
 
 void setup(){
   Serial.begin(9600);
@@ -82,158 +82,176 @@ void setup(){
   digitalWrite(sensor, HIGH);
   pinMode(push_btn, INPUT);
   digitalWrite(push_btn, HIGH);
+  
+  pinMode(primary_power, INPUT);
 
   myservo.write(70);
   Serial.println("Ready");
 }
 
 void loop(){
-  EthernetClient client = server.available();
-  if(client){
-    while(client.connected()){
-      if(client.available()){
-        if(client.find("GET /")){
-          while(client.findUntil("command_", "\n\r")){
-            if(!disp_online){
-              disp_online = true;
-              disp_offline = false;
-              lcd_offline(false);
-            }  
-            char type = client.read();
-            if(type == 'o') {
-              Serial.println("Open Command");
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-Type: application/json");
-              client.println();
-              client.print("{\"response\":\"open\"}");
-              open_door();
-            }
-            else if(type == 's'){
-              int val = client.parseInt();
-              Serial.println("Status Command");
-              if(val == 2){
-                device_status = true;
-                lcd_init();
+  if(analogRead(primary_power) > 300){
+    if(backup_power){
+      backup_power = false;
+      while(digitalRead(sensor) == HIGH);
+      delay(1000);
+      myservo.write(70);
+      lcd_init();
+    }
+    EthernetClient client = server.available();
+    if(client){
+      while(client.connected()){
+        if(client.available()){
+          if(client.find("GET /")){
+            while(client.findUntil("command_", "\n\r")){
+              if(!disp_online){
+                disp_online = true;
+                disp_offline = false;
+                lcd_offline(false);
+              }  
+              char type = client.read();
+              if(type == 'o') {
+                Serial.println("Open Command");
                 client.println("HTTP/1.1 200 OK");
                 client.println("Content-Type: application/json");
                 client.println();
-                client.print("{\"response\":\"activate\"}");
+                client.print("{\"response\":\"open\"}");
+                open_door();
               }
-              else if(val == 0){
-                device_status = false;
-                Serial.println("Deactivate");
+              else if(type == 's'){
+                int val = client.parseInt();
+                Serial.println("Status Command");
+                if(val == 2){
+                  device_status = true;
+                  lcd_init();
+                  client.println("HTTP/1.1 200 OK");
+                  client.println("Content-Type: application/json");
+                  client.println();
+                  client.print("{\"response\":\"activate\"}");
+                }
+                else if(val == 0){
+                  device_status = false;
+                  Serial.println("Deactivate");
+                  client.println("HTTP/1.1 200 OK");
+                  client.println("Content-Type: application/json");
+                  client.println();
+                  client.print("{\"response\":\"deactivate\"}");
+                }
+                else{
+                  Serial.println("Unexpected command");
+                  client.println("HTTP/1.1 200 OK");
+                  client.println("Content-Type: application/json");
+                  client.println();
+                  client.print("{\"response\":\"invalid_status\"}");
+                }
+              }
+              else if(type == 'a') {
+                Serial.println("Pinging");
                 client.println("HTTP/1.1 200 OK");
                 client.println("Content-Type: application/json");
                 client.println();
-                client.print("{\"response\":\"deactivate\"}");
+                client.print("{\"response\":\"online\"}");
               }
-              else{
-                Serial.println("Unexpected command");
+              else if(type == 'c') {
+                Serial.println("Check Status");
                 client.println("HTTP/1.1 200 OK");
                 client.println("Content-Type: application/json");
                 client.println();
-                client.print("{\"response\":\"invalid_status\"}");
+                if(device_status){
+                  client.print("{\"response\":\"active\"}");
+                }
+                else{
+                  client.print("{\"response\":\"inactive\"}");
+                }
               }
-            }
-            else if(type == 'a') {
-              Serial.println("Pinging");
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-Type: application/json");
-              client.println();
-              client.print("{\"response\":\"online\"}");
-            }
-            else if(type == 'c') {
-              Serial.println("Check Status");
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-Type: application/json");
-              client.println();
-              if(device_status){
-                client.print("{\"response\":\"active\"}");
+              else if(type == 'f') {
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-Type: application/json");
+                client.println();
+                client.print("{\"response\":\"false\"}");
+                wrong_password();
               }
-              else{
-                client.print("{\"response\":\"inactive\"}");
+              else {
+                Serial.print("Unexpected type ");
+                Serial.print(type);
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-Type: application/json");
+                client.println();
+                client.print("{\"response\":\"invalid\"}");
               }
-            }
-            else if(type == 'f') {
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-Type: application/json");
-              client.println();
-              client.print("{\"response\":\"false\"}");
-              wrong_password();
-            }
-            else {
-              Serial.print("Unexpected type ");
-              Serial.print(type);
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-Type: application/json");
-              client.println();
-              client.print("{\"response\":\"invalid\"}");
             }
           }
+          break;
         }
-        break;
+      }
+      delay(10);
+      client.stop();
+    }
+  
+    if(digitalRead(push_btn) == LOW){
+      auth_user("keluar");
+      open_door();
+    }
+  
+    if(device_status){
+      if(!device_isLock()){
+        char key = keypad.getKey();
+        if (key != NO_KEY && key != '*' && key != '#'){
+          idleTime = millis();
+          isInputting = true;
+          disp += "*";
+          lcd.setCursor(0,2);
+          lcd.print(disp);
+          pass[count] = key;
+          count++;
+        }
+        if(key == '*'){
+          idleTime = millis();
+          isInputting = false;
+          attempt++;
+          auth_user(pass);
+          sys_init();
+        }
+        if(key == '#'){
+          idleTime = millis();
+          sys_init();
+        }
+        if(count > 9){
+          auth_user(pass);
+          sys_init();
+        }
       }
     }
-    delay(10);
-    client.stop();
-  }
-
-  if(digitalRead(push_btn) == LOW){
-    auth_user("keluar");
-    open_door();
-  }
-
-  if(device_status){
-    if(!device_isLock()){
-      char key = keypad.getKey();
-      if (key != NO_KEY && key != '*' && key != '#'){
-        idleTime = millis();
-        isInputting = true;
-        disp += "*";
-        lcd.setCursor(0,2);
-        lcd.print(disp);
-        pass[count] = key;
-        count++;
-      }
-      if(key == '*'){
-        idleTime = millis();
-        isInputting = false;
-        attempt++;
-        auth_user(pass);
-        sys_init();
-      }
-      if(key == '#'){
-        idleTime = millis();
-        sys_init();
-      }
-      if(count > 9){
-        auth_user(pass);
-        sys_init();
+    else{
+      if(!disp_inactive){
+        disp_inactive = true;
+        lcd_print("Perangkat Non-Aktif");
       }
     }
-  }
-  else{
-    if(!disp_inactive){
-      disp_inactive = true;
-      lcd_print("Perangkat Non-Aktif");
+  
+    unsigned long curTime = millis();
+    if(curTime - idleTime > interval_idle){
+      idleTime = curTime;
+      isInputting = false;
+      sys_init();
     }
-  }
-
-  unsigned long curTime = millis();
-  if(curTime - idleTime > interval_idle){
-    idleTime = curTime;
-    isInputting = false;
-    sys_init();
-  }
-
-  if(!isInputting){
-    if(curTime - prevTime > interval_reset){
-      prevTime = curTime;
+  
+    if(!isInputting){
+      if(curTime - prevTime > interval_reset){
+        prevTime = curTime;
+        attempt = 0;
+        lcd.setCursor(0,3);
+        for(int i=0;i<20;i++){
+          lcd.print(" ");
+        }
+      }
+    }
+  }else{
+    if(!backup_power){
+      backup_power = true;
+      lcd_print("Pintu Terbuka");
       attempt = 0;
-      lcd.setCursor(0,3);
-      for(int i=0;i<20;i++){
-        lcd.print(" ");
-      }
+      myservo.write(30);
     }
   }
 }
